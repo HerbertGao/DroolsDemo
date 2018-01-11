cc7f80ae6d8a1c1da2485753f77435edee78daa3
HerbertGao <gaohaobo1994@gmail.com>
2018-01-11 15:16:37 +0800

    DroolsDemo

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,12 +1,19 @@
 package demo;
 
-import org.kie.api.KieServices;
-import org.kie.api.runtime.KieContainer;
+import org.drools.core.impl.InternalKnowledgeBase;
+import org.drools.core.impl.KnowledgeBaseFactory;
+import org.kie.api.definition.KiePackage;
+import org.kie.api.io.ResourceType;
 import org.kie.api.runtime.KieSession;
+import org.kie.internal.builder.KnowledgeBuilder;
+import org.kie.internal.builder.KnowledgeBuilderFactory;
+import org.kie.internal.io.ResourceFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class Main {
@@ -21,14 +28,30 @@ public class Main {
     }
 
     private static void runDrools() throws IOException {
-        KieServices ks = KieServices.Factory.get();
-        KieContainer kieContainer = ks.getKieClasspathContainer();
-        KieSession kieSession = kieContainer.newKieSession("demo_ksession");
+        InternalKnowledgeBase knowledgeBase = KnowledgeBaseFactory.newKnowledgeBase();
+        KnowledgeBuilder knowledgeBuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+
+        knowledgeBuilder.add(ResourceFactory.newClassPathResource("rules/demo.drl", Main.class), ResourceType.DRL);
+        if (knowledgeBuilder.hasErrors()) {
+            System.out.println(knowledgeBuilder.getErrors().toString());
+            throw new IOException("Can't load demo.drl");
+        }
+
+        Collection<KiePackage> pkgs = knowledgeBuilder.getKnowledgePackages();
+        knowledgeBase.addPackages(pkgs);
+        KieSession kieSession = knowledgeBase.newKieSession();
 
         Message message = new Message();
         message.setStatus(Message.HELLO);
         kieSession.insert(message);
         kieSession.fireAllRules();
         kieSession.dispose();
+
+        System.out.println("Input 'y' to continue");
+        Scanner s = new Scanner(System.in);
+        String in = s.nextLine();
+        if (in.equals("y")) {
+            runDrools();
+        }
     }
 }
